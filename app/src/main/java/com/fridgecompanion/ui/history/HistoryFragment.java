17136eb818926de1ebc4b04e874808c1bf1b172d
andres-a-h <andres.a.h@outlook.com>
2021-02-28 17:59:08 -0800

    added basic ui, buttons, listview

--- app/src/main/java/com/fridgecompanion/ui/history/HistoryFragment.java
+++ app/src/main/java/com/fridgecompanion/ui/history/HistoryFragment.java
@@ -1,10 +1,17 @@
 package com.fridgecompanion.ui.history;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,24 +19,104 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.fridgecompanion.Food;
 import com.fridgecompanion.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class HistoryFragment extends Fragment {
 
     private HistoryViewModel historyViewModel;
+    private ArrayList<Food> foodList;
+    private Context context;
+    private ListView listView;
+    private Calendar DateAndTime;
+    private String[] foodNames = new String[]{"apple", "orange", "chocolate"};
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        foodList = new ArrayList<Food>(3);
+        // TODO: update for show and tell 3
+        for (int i = 0; i < foodNames.length; i++) {
+            Food food = new Food();
+            food.setFoodName(foodNames[i]);
+            DateAndTime = Calendar.getInstance();
+            long now = DateAndTime.getTimeInMillis();
+            food.setEnteredDate(now);
+            foodList.add(food);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        historyViewModel =
-                new ViewModelProvider(this).get(HistoryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_history, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        historyViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        setHasOptionsMenu(true);
+        listView = (ListView) view.findViewById(R.id.history_list);
+        context = view.getContext();
+        updateHistory();
+        return view;
+    }
+
+    /**
+     * @Override Method: onCreateOptionsMenu
+     * Notes: Create the clickable menu item to allow the user to delete all history entries
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.delete_all, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    /**
+     * @Override Method: onOptionsItemSelected
+     * Notes: When the menu item is clicked, delete all history entries
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                new Thread(() -> {
+                    // Delete history
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getContext(), "Deleted history", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }).start();
+                break;
+            default:
+                // Not supposed to be here
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Method: updateHistory
+     * Notes: Update the history entries displayed to the user
+     */
+    public void updateHistory() {
+        new Thread(() -> {
+            // Do stuff
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    HistoryListAdapter historyListAdapter = new HistoryListAdapter(getContext(), foodList);
+                    listView.setAdapter(historyListAdapter);
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            // Do something when clicked
+                        }
+                    });
+                }
+            });
+        }).start();
     }
 }
---
+++ app/src/main/java/com/fridgecompanion/ui/history/HistoryListAdapter.java
@@ -0,0 +1,62 @@
+package com.fridgecompanion.ui.history;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.fridgecompanion.Food;
+import com.fridgecompanion.R;
+
+import java.util.ArrayList;
+
+public class HistoryListAdapter extends BaseAdapter {
+
+    private ArrayList<Food> foodList;
+    private Context context;
+
+    /**
+     * Method: HistoryListAdapter
+     * Notes: Constructor
+     */
+    public HistoryListAdapter(Context context, ArrayList<Food> foodList) {
+        this.context = context;
+        this.foodList = foodList;
+    }
+
+    @Override
+    public int getCount() {
+        return foodList.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return foodList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (null == view) {
+            view = LayoutInflater.from(context).inflate(R.layout.my_custom_list_item,
+                    parent, false);
+        }
+
+        // Get the first row from the custom list layout
+        TextView firstRow = (TextView) view.findViewById(R.id.id_history_first_row);
+
+        // Load the text into the text view
+        String foodName = foodList.get(position).getFoodName();
+        String date = String.valueOf(foodList.get(position).getEnteredDate());
+        firstRow.setText("User added " + foodName + " to fridge. " + date);
+
+        return view;
+    }
+
+}
